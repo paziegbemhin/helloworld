b29e0260751b815b5d6928effa0ca947844ff8d9
paziegbemhin <66569396+paziegbemhin@users.noreply.github.com>
2022-07-29 14:46:38 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,10 @@
+# first Python program
+def hello(msg):
+    #  works only on Python 3.7+
+    
+    print("hello {msg}!")
+    
+  if _name_ == "_main_":
+    hello("World") 
+    hello("Alice")  
+    hello("Bob")   
